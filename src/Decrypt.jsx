60ccc0ee9d620dfef2c1cd2becf522c286597dec
CarlosAmul <60094736@cna-qatar.edu.qa>
2024-06-15 01:55:40 +0300

    Removed modal

--- src/Decrypt.jsx
+++ src/Decrypt.jsx
@@ -2,7 +2,6 @@ import { useState } from "react"
 import CryptoJS from "crypto-js"
 import { FaEye, FaEyeSlash } from "react-icons/fa";
 import { Button, Form, FormControl, FormGroup, InputGroup } from "react-bootstrap"
-import DecryptedModal from "./Modals/DecryptedModal";
 
 export default function Decrypt() {
 
@@ -22,10 +21,8 @@ export default function Decrypt() {
     const [secret, setSecret] = useState("")
     const [mode, setMode] = useState("ECB")
     const [padding, setPadding] = useState("PKCS5")
-    const [output, setOutput] = useState("")
 
     const [hideSecret, setHideSecret] = useState(true)
-    const [showOutput, setShowOutput] = useState(false)
 
     function decrypt() {
         var iv = CryptoJS.enc.Base64.parse(initVector)
@@ -38,14 +35,17 @@ export default function Decrypt() {
             mode: cipherModes[mode],
             padding: paddingOptions[padding]
         })
-
-        setOutput(decrypted.toString(CryptoJS.enc.Utf8))
         copyToClipboard(decrypted.toString(CryptoJS.enc.Utf8))
     }
 
     function copyToClipboard(text) {
-        navigator.clipboard.writeText(text)
-        alert("Copied to clipboard!")
+
+        navigator.clipboard.writeText(text).then(() => {
+            alert("Copied to clipboard!")
+        }).catch((err) => {
+            alert("Error copying to clipboard")
+        })
+        
     }
 
     return (
@@ -90,9 +90,7 @@ export default function Decrypt() {
                 </Form.Group>
 
                 <Button variant="dark" onClick={decrypt}>Decrypt</Button>
-                {output && <Button variant="link" onClick={()=>setShowOutput(true)}>Output not in clipboard?</Button>}
             </Form>
-            <DecryptedModal output={output} clear={()=>setOutput("")} open={showOutput} setOpen={setShowOutput} />
         </div>
     )
 
